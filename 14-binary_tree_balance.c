959a274ccc0aaf3bfc2a0c6e0e20fa9dcad694f3
OuardaHolberton <12985@holbertonstudents.com>
2026-04-10 07:54:59 +0200

    correction betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,17 +14,11 @@ static size_t binary_tree_height_local(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		height_left = 1 + binary_tree_height_local(tree->left);
-	else
-		height_left = 0;
-	if (tree->right != NULL)
-		height_right = 1 + binary_tree_height_local(tree->right);
-	else
-		height_right = 0;
+	height_left = binary_tree_height_local(tree->left);
+	height_right = binary_tree_height_local(tree->right);
 	if (height_left > height_right)
-		return (height_left);
-	return (height_right);
+		return (height_left + 1);
+	return (height_right + 1);
 }
 
 /**
